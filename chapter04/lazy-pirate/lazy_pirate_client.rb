369433d4e86ebd964c5a8159ac2dc6cd50f6166c
blooberr <blooberr@gmail.com>
2013-10-02 23:54:12 -0700

    adding lazy pirate client and server example (initial commit)

---
+++ chapter04/lazy-pirate/lazy_pirate_client.rb
@@ -0,0 +1,68 @@
+require 'ffi-rzmq'
+require 'logger'
+
+module LazyPirate
+  class Client
+    attr_accessor :logger, :retries, :server_port, :timeout
+    def initialize(
+      logger:      Logger.new(STDOUT),
+      retries:     3,
+      server_port: 5555,
+      timeout:     5 * 1000) # in milliseconds
+
+      @logger      = logger
+      @retries     = retries
+      @server_port = server_port
+      @timeout     = timeout
+
+      @context = ZMQ::Context.new(1)
+      @poller = ZMQ::Poller.new
+      reconnect
+    end
+
+    def reconnect
+      if @client_socket
+        @poller.deregister @client_socket, ZMQ::POLLIN
+        @client_socket.close
+        logger.info "Reconnecting ... "
+      end
+
+      @client_socket = @context.socket(ZMQ::REQ)
+      @client_socket.setsockopt(ZMQ::LINGER, 0)
+      @client_socket.connect("tcp://localhost:#{@server_port}")
+      @poller.register @client_socket, ZMQ::POLLIN
+    end
+
+    def send
+      retries.times do |tries|
+        message = "message - ##{tries}"
+        send_status = @client_socket.send_string(message)
+        rc = @poller.poll(@timeout)
+        if rc > 0
+          @poller.readables.each do |socket|
+            socket.recv_string(m="")
+            puts "I: Server replied OK- #{m.inspect}"
+          end
+          return
+        else
+          reconnect
+        end
+        logger.info "W: no response from server, retry ##{tries}"
+      end
+      raise "E: server seems to be offline, abandoning!"
+    end
+
+    def run
+      loop do
+        send
+      end
+    end
+  
+    ##---
+  end
+end
+
+lpc = LazyPirate::Client.new
+lpc.run
+
+
---
+++ chapter04/lazy-pirate/lazy_pirate_server.rb
@@ -0,0 +1,49 @@
+require 'ffi-rzmq'
+require 'logger'
+
+module LazyPirate
+  class Server
+    attr_accessor :max_cycles, :server_port, :logger
+    def initialize(
+      logger:     Logger.new(STDOUT), 
+      max_cycles: 3,
+      server_port: 5555)
+
+      @logger      = logger
+      @max_cycles  = max_cycles
+      @server_port = server_port
+    end
+
+    def connect
+      @context = ZMQ::Context.new(1)
+      @server_socket  = @context.socket(ZMQ::REP)
+      @server_socket.bind("tcp://*:#{server_port}") 
+    end
+
+    def run
+      connect
+      cycles = 0
+
+      loop do
+        @server_socket.recv_string(message="")
+        cycles = cycles + 1
+        if (cycles > max_cycles) && (rand(3) == 0)  
+          logger.info "I: simulating a crash"
+          break
+        else
+          if (cycles > max_cycles) && (rand(3) == 0) 
+            logger.info "I: simulating CPU overload"
+            sleep 2
+          end
+          logger.info "I: normal request - #{message}"
+          sleep 1 # simulate heavy work
+          @server_socket.send_string message
+        end  
+      end
+    end
+    ##--
+  end
+end
+
+lps = LazyPirate::Server.new
+lps.run
